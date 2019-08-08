e759986944e3da2bc979114aee93fb8f1aaab012
mireille <mimimireille95@gmail.com>
2019-08-08 07:40:24 -0400

    java added

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function Wedding(firstName,lastName, phone,country, email,eventDetails,date,eventType, number,preferedVenues,price) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phone = phone;
+    this.country = country;
+    this.email = email;
+    this.eventDetails = eventDetails;
+    
+    this.date = date;
+    this. eventType = eventType;
+    this. number = number;
+    this. preferedVenues = preferedVenues;
+    this.price = price;
+   
+  }
